67092f998682c7c7ea3df77a647377b870a9af9a
BenChouUni <ben900202chou@gmail.com>
2022-02-23 01:37:31 +0800

    CardStructureBuild
    卡牌基本架構完成

--- Cardgame_1/Assets/Scripts/CardDisplay.cs
+++ Cardgame_1/Assets/Scripts/CardDisplay.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardDisplay : MonoBehaviour
 {
+    public Text nameText;
+    public Text attackText;
+    public Text healthText;
+    public Text effectText;
+
+    public Image backgroundImage;
+
+    public Card card;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +24,28 @@ public class CardDisplay : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ShowCard()
+    {
+        nameText.text = card.cardName;
+        if (card is MonsterCard)
+        {
+            var monster = card as MonsterCard;
+
+            attackText.text = monster.attack.ToString();
+            healthText.text = monster.healthPoint.ToString();
+
+            effectText.gameObject.SetActive(false);
+        }
+        else if (card is SpellCard)
+        {
+            var spell = card as SpellCard;
+
+            effectText.text = spell.effect.ToString();
+
+            attackText.gameObject.SetActive(false);
+            healthText.gameObject.SetActive(false);
+        }
     }
 }
