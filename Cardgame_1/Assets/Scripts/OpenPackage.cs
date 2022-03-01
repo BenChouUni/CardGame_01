b3447c2cbbdfc621767552235e3f052ce130f6c2
BenChouUni <ben900202chou@gmail.com>
2022-03-01 17:25:27 +0800

    CardDrawing
    抽卡基本架構完成

--- Cardgame_1/Assets/Scripts/OpenPackage.cs
+++ Cardgame_1/Assets/Scripts/OpenPackage.cs
@@ -7,11 +7,12 @@ public class OpenPackage : MonoBehaviour
 {
     public int drawCardNumber = 5;
 
-    public GameObject Canvas;
+    public GameObject CardPool;
     public GameObject cardPrefab;
 
 
     CardStore cardStore;
+    List<GameObject> cards = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -27,16 +28,28 @@ public class OpenPackage : MonoBehaviour
 
     public void OnclickOpen()
     {
+        ClearPool();
         for (int i = 0; i < drawCardNumber; i++)
         {
             GameObject newCard = GameObject.Instantiate(cardPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-            newCard.transform.SetParent(Canvas.transform, false);
+            newCard.transform.SetParent(CardPool.transform, false);
 
             newCard.GetComponent<CardDisplay>().card = cardStore.RandomCard();
+
+            cards.Add(newCard);
            // string cardName = newCard.transform.GetChild(1).gameObject.GetComponent<Text>().text;
             //Debug.Log("抽到" + cardName);//嘗試檢查抽到卡牌
         }
 
        
+    }
+
+    public void ClearPool()
+    {
+        foreach (var card in cards)
+        {
+            Destroy(card);
+        }
+
     }
 }
